e707a8d3fab2c545e531b6ff4ea68971ec326388
Pedro Oliveira <60803315+PedroVIOliv@users.noreply.github.com>
2024-09-03 15:56:23 -0300

    feat: different colors on graph and problem with 21 delivery points added

--- visualizar_melhor_caminho.py
+++ visualizar_melhor_caminho.py
@@ -1,6 +1,8 @@
 import json
 import matplotlib.pyplot as plt
 from adjustText import adjust_text
+import numpy as np
+import matplotlib.colors as mcolors
 
 # Carregar o arquivo JSON
 with open('results/best_path_graph.json') as f:
@@ -15,6 +17,7 @@ best_fitness = data["best_fitness"]
 best_fitness_rounded = round(best_fitness, 2)
 
 # Adicionar o ponto base (0) ao início e ao final do caminho
+num_subroutes = best_path.count(0)
 best_path = [0] + best_path + [0]
 
 # Extrair coordenadas na ordem do melhor caminho
@@ -28,21 +31,41 @@ pesos = [coord["peso"] for coord in ordered_coordinates]
 # Criar a figura e o eixo
 fig, ax = plt.subplots(figsize=(10, 6))
 
-# Plotar os pontos de entrega
-ax.plot(x_values[1:-1], y_values[1:-1], marker='o', color='blue', label='Pontos de Entrega')
-
 # Plotar o primeiro ponto do objeto "coordinates" em vermelho com marcador de quadrado
 first_coordinate = coordinates[0]
 ax.plot(first_coordinate['x'], first_coordinate['y'], marker='s', color='red', label='Base Inicial')
+# Definir colormap
+colors = plt.cm.rainbow(np.linspace(0, 1, num_subroutes + 1))
+
+# Plotar segmentos com a cor correspondente de cada subrota
+color_index = 0
+current_color = colors[color_index]
 
 # Adicionar números e setas entre os pontos para indicar o caminho percorrido
 for i in range(len(best_path) - 1):
     x_start, y_start = x_values[i], y_values[i]
     x_end, y_end = x_values[i + 1], y_values[i + 1]
     
-    # Adicionar setas pequenas
-    ax.annotate('', xy=(x_end, y_end), xytext=(x_start, y_start),
-                arrowprops=dict(arrowstyle='->', color='blue', lw=1.5))
+    # Plotar segmento
+    ax.plot([x_start, x_end], [y_start, y_end], marker='o', color=current_color, lw=2,
+            label=f'Subrota {color_index+1}' if i == 0 or best_path[i] == 0 else "")
+    
+    # Cor de seta escura
+    darker_color = mcolors.to_rgba(current_color, alpha=1.0)
+    darker_color = (darker_color[0] * 0.7, darker_color[1] * 0.7, darker_color[2] * 0.7, darker_color[3])
+    arrow_x = (x_start + x_end) / 2
+    arrow_y = (y_start + y_end) / 2
+    dx = x_end - x_start
+    dy = y_end - y_start
+    ax.annotate('', xy=(arrow_x + dx * 0.001, arrow_y + dy * 0.001), xytext=(arrow_x, arrow_y),
+                arrowprops=dict(arrowstyle="-|>", color=darker_color, lw=2),
+                annotation_clip=False)
+
+    # Muda de cor ao voltar pra base
+    if best_path[i + 1] == 0:
+        color_index += 1
+        if color_index <= num_subroutes:
+            current_color = colors[color_index]
 
 texts = []
 for i, (x, y, peso, idx) in enumerate(zip(x_values, y_values, pesos, best_path)):
